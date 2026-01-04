0b07793529e779a7e7fd8cd50083ee41df82622d
sergey-prochukhan <65830968+sergey-prochukhan@users.noreply.github.com>
2026-01-04 15:36:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ menu.add(ansyes,ansno,anselse)
 
 @bot.message_handler(commands=['start'])
 def start_message (message):
-	bot.send_message(message.chat.id, "Тестовый бот для проверки системы хостинга чат ботов Bothost в связке с github. Для оценки работы сервиса отправьте ответ на вопрос: Видите ли вы это сообщение и меню с конпками?", reply_markup = menu)
+	bot.send_message(message.chat.id, "Тестовый бот для проверки системы хостинга чат ботов Bothost в связке с github. Для оценки работы сервиса отправьте ответ на вопрос: Видите ли вы это сообщение и меню с кнопками?", reply_markup = menu)
 
 @bot.message_handler(content_types=['text'])
 def text_messages(message):
@@ -31,6 +31,7 @@ def forward(message):
 bot.infinity_polling()
 
 #Test
+
 
 
 
