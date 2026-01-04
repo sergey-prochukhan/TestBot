3b8a0fcd27e27b95c4b99d4e7cbc7c35f27bd8df
sergey-prochukhan <65830968+sergey-prochukhan@users.noreply.github.com>
2026-01-04 14:54:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ menu.add(ansyes,ansno,anselse)
 def start_message (message):
 	bot.send_message(message.chat.id, "Тестовый бот для проверки системы хостинга чат ботов Bothost в связке с github. Для оценки работы сервиса отправьте ответ на вопрос: Видите ли вы это сообщение и меню с конпками?", reply_markup = menu)
 
+@bot.message_handler(content_types=['text'])
 def text_messages(message):
 	if message.text == "Да, всё отлично":
 		bot.send_message(message.chat.id, "Отлично! Спасибо что уделили время", forward)
@@ -30,6 +31,7 @@ def forward(message):
 bot.infinity_polling()
 
 #Test
+
 
 
 
