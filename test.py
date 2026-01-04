68360c63a22f18aea840c5197bccd07d369652d0
sergey-prochukhan <65830968+sergey-prochukhan@users.noreply.github.com>
2026-01-04 12:55:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ menu = types.ReplyKeyboardMarkup (resize_keyboard=True)
 ansyes = types.KeyboardButton ("Да")
 ansno = types.KeyboardButton ("Нет")
 anselse = types.KeyboardButton ("Иное")
-menu.add(ansyes,ansno,ansyes)
+menu.add(ansyes,ansno,anselse)
 
 @bot.message_handler(commands=['start'])
 def start_message (message):
 	bot.send_message(message.chat.id, "Тестовый бот для проверки системы хостинга чат ботов Bothost в связке с github. Для оценки работы сервиса отправьте ответ на вопрос: Видите ли вы это сообщение и меню с конпками?", reply_markup = menu)
 
 bot.infinity_polling()
+
 
