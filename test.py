9aa8f5d7f39c2c78535b1cd65afde28623c6a6e3
sergey-prochukhan <65830968+sergey-prochukhan@users.noreply.github.com>
2026-01-04 16:17:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+import telegram.ext
+from telegram.ext import Updater, CallbackContext
 import telebot
 from telebot import types
+
+u = Updater(config.TOKEN, use_context=True)
+j = u.job_queue
 
 bot = telebot.TeleBot('8513847851:AAG3PzwcGTbYO25syg-vsEWtki59dVGgwl8')
 
@@ -17,6 +22,8 @@ def start_message (message):
 def text_messages(message):
 	if message.text == "Да, всё отлично":
 		bot.send_message(message.chat.id, "Отлично! Спасибо что уделили время", reply_markup=types.ReplyKeyboardRemove())
+		j.run_once(callback_1, 5, context=message.chat.id)
+		
 	elif message.text == "Нет":
 		bot.send_message(message.chat.id, "Опишите поподробнее, что пошло не так:", reply_markup=types.ReplyKeyboardRemove())
 		bot.register_next_step_handler(message, forward)
@@ -27,10 +34,14 @@ def text_messages(message):
 def forward(message):
 	bot.forward_message(chat_id='@Test_Answer_b', from_chat_id = message.chat.id, message_id=message.id)
 	bot.send_message(message.chat.id, "Спасибо, что уделили время.", reply_markup=menu)
+
+def callback_1(context: CallbackContext):
+    context.bot.send_message(chat_id=context.job.context, text='Это сообщение отправлено с задержкой в 5 секунд')
 	
 bot.infinity_polling()
 
 #Test
+
 
 
 
