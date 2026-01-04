c3d0599740c118fd8835671946fa93422fff6e5c
sergey-prochukhan <65830968+sergey-prochukhan@users.noreply.github.com>
2026-01-04 11:58:58 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot('8513847851:AAG3PzwcGTbYO25syg-vsEWtki59dVGgwl8')
 
 @bot.message_handler(commands=['start'])
 def start_message (message):
-	bot.send_message(message.chat.id, "Тест бота")
+	bot.send_message(message.chat.id, "Тестовый бот для проверки системы хостинга чат ботов Bothost в связке с github")
 
 bot.infinity_polling()
