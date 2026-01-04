2521b200c189ca4757ad32cd27648f3c2c83f064
sergey-prochukhan <65830968+sergey-prochukhan@users.noreply.github.com>
2026-01-04 12:53:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from telebot import types
 bot = telebot.TeleBot('8513847851:AAG3PzwcGTbYO25syg-vsEWtki59dVGgwl8')
 
 menu = types.ReplyKeyboardMarkup (resize_keyboard=True)
-ansyes = types.ReplyKeyboardButton ("Да")
-ansno = types.ReplyKeyboardButton ("Нет")
-anselse = types.ReplyKeyboardButton ("Иное")
+ansyes = types.KeyboardButton ("Да")
+ansno = types.KeyboardButton ("Нет")
+anselse = types.KeyboardButton ("Иное")
 menu.add(ansyes,ansno,ansyes)
 
 @bot.message_handler(commands=['start'])
@@ -14,3 +14,4 @@ def start_message (message):
 	bot.send_message(message.chat.id, "Тестовый бот для проверки системы хостинга чат ботов Bothost в связке с github. Для оценки работы сервиса отправьте ответ на вопрос: Видите ли вы это сообщение и меню с конпками?", reply_markup = menu)
 
 bot.infinity_polling()
+
