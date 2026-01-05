af0c7498d4c7a0fc45d953af8f016e9b8d6bf08b
sergey-prochukhan <65830968+sergey-prochukhan@users.noreply.github.com>
2026-01-05 12:13:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ bot = telebot.TeleBot(BOT_KEY)
 
 
 menu = types.InlineKeyboardMarkup ()
-menu.add(InlineKeyboardButton('Да, всё отлично!', callback_data('Да, всё отлично!')
-menu.add(InlineKeyboardButton('Нет', callback_data('Нет')
-menu.add(InlineKeyboardButton('Иное', callback_data('Иное')							  
+menu.add(InlineKeyboardButton('Да, всё отлично!', callback_data('Да, всё отлично!'))
+menu.add(InlineKeyboardButton('Нет', callback_data('Нет'))
+menu.add(InlineKeyboardButton('Иное', callback_data('Иное'))							  
 
 '''ansyes = types.KeyboardButton ("Да, всё отлично")
 ansno = types.KeyboardButton ("Нет")
@@ -44,6 +44,7 @@ def forward(message):
 bot.infinity_polling()
 
 #Test
+
 
 
 
